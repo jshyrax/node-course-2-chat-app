287d4987c65e7e791803b9d8956ef40116efc9b2
jshyrax <willemmarx@outlook.com>
2018-01-23 16:00:40 +1300

    Add more UI features

--- public/js/index.js
+++ public/js/index.js
@@ -44,16 +44,16 @@ locationButton.on('click', function (e) {
         return alert('Geolocation not supported by your browser');
     }
 
-    locationButton.attr('disabled', 'disabled');
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
 
     navigator.geolocation.getCurrentPosition( function (position){
-        locationButton.removeAttr('disabled');
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function (err){
-        locationButton.removeAttr('disabled');
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
 });
